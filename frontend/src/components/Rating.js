6ae8d0d2ff2274bf8f4e1620fe2f26331f29c00c
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2021-09-14 18:14:20 +0200

    8. Rating Component

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,71 @@
+function Rating({ value, text, color }) {
+  return (
+    <div className='rating'>
+      {/* pierwsza gwiazda */}
+      <span>
+        <i style={{ color }} className={
+          value >= 1
+            ? 'fas fa-star'
+            : value >= 0.5
+              ? 'fas fa-start-half-alt'
+              : 'far fa-star'
+        }>
+        </i>
+      </span>
+
+      {/* druga gwiazdka */}
+      <span>
+        <i style={{ color }} className={
+          value >= 2
+            ? 'fas fa-star'
+            : value >= 1.5
+              ? 'fas fa-start-half-alt'
+              : 'far fa-star'
+        }>
+        </i>
+      </span>
+
+      {/* trzecia gwiazdka */}
+      <span>
+        <i style={{ color }} className={
+          value >= 3
+            ? 'fas fa-star'
+            : value >= 2.5
+              ? 'fas fa-start-half-alt'
+              : 'far fa-star'
+        }>
+        </i>
+      </span>
+
+      {/* czwarta gwiazdka */}
+      <span>
+        <i style={{ color }} className={
+          value >= 4
+            ? 'fas fa-star'
+            : value >= 3.5
+              ? 'fas fa-start-half-alt'
+              : 'far fa-star'
+        }>
+        </i>
+      </span>
+
+      {/* piąta gwiazdka */}
+      <span>
+        <i style={{ color }} className={
+          value >= 5
+            ? 'fas fa-star'
+            : value >= 4.5
+              ? 'fas fa-start-half-alt'
+              : 'far fa-star'
+        }>
+        </i>
+      </span>
+
+      {/* jeśli teks isntije eto go zwróc */}
+      <span>{text && text}</span>
+
+    </div>
+  )
+}
+
+export default Rating
